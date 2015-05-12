b3a8395ef867721d7985e020f7afc87f09c549fc
urokuta <takuro.mizobe@gmail.com>
2015-05-13 00:22:56 +0900

    create github org stats

---
+++ sample.rb
@@ -0,0 +1,8 @@
+require './github_org_stats'
+gos = GithubOrgStats.new(access_token: "your token", org_name: "your org")
+puts "fetching last week your org commits..."
+last_week = gos.summary_commits_all_repo(week: 1)
+puts last_week.inspect
+puts "fetching this week your org commits..."
+this_week = gos.summary_commits_all_repo
+puts this_week.inspect
